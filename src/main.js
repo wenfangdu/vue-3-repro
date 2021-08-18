1414482888e71ffbbf665c8d8c80ef01192f1331
Wenfang Du <no_reply_mail@163.com>
2021-08-18 19:50:23 +0800

    finished

--- src/main.js
+++ src/main.js
@@ -2,5 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import installElementPlus from './plugins/element.js'
+import locale from 'element-plus/lib/locale/lang/zh-cn.js'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp({
+  render() {
+    return (
+      <ElConfigProvider locale={locale}>
+        <App />
+      </ElConfigProvider>
+    )
+  },
+})
+installElementPlus(app)
+app.use(store).use(router).mount('#app')
